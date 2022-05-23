e5e5e7e22cb9e5507a737b1620b69f4377d9fb9e
Juliana Leshchenko <68852795+JuliianaLeshchenko@users.noreply.github.com>
2022-05-23 13:15:25 +0200

    Update CZ_Lamproderma_SDM.R

--- CZ_Lamproderma_SDM.R
+++ CZ_Lamproderma_SDM.R
@@ -126,3 +126,95 @@ M <- cor(df_corr) # correlation matrix
 #corrplot(M, method="square", type = "upper")
 #corrplot(M, method="color", type = "upper")
 corrplot(M, method = "square", order = "hclust", addrect = 9)
+### CONFERENCE in POLAND
+#import preloaded data from worldclim:
+bio1 <- raster("wc2.1_30s_bio_1.tif")
+bio2 <- raster("wc2.1_30s_bio_2.tif")
+bio3 <- raster("wc2.1_30s_bio_3.tif")
+bio4 <- raster("wc2.1_30s_bio_4.tif")
+bio5 <- raster("wc2.1_30s_bio_5.tif")
+bio6 <- raster("wc2.1_30s_bio_6.tif")
+bio7 <- raster("wc2.1_30s_bio_7.tif")
+bio8 <- raster("wc2.1_30s_bio_8.tif")
+bio9 <- raster("wc2.1_30s_bio_9.tif")
+bio10 <- raster("wc2.1_30s_bio_10.tif")
+bio11 <- raster("wc2.1_30s_bio_11.tif")
+bio12 <- raster("wc2.1_30s_bio_12.tif")
+bio13 <- raster("wc2.1_30s_bio_13.tif")
+bio14 <- raster("wc2.1_30s_bio_14.tif")
+bio15 <- raster("wc2.1_30s_bio_15.tif")
+bio16 <- raster("wc2.1_30s_bio_16.tif")
+bio17 <- raster("wc2.1_30s_bio_17.tif")
+bio18 <- raster("wc2.1_30s_bio_18.tif")
+bio19 <- raster("wc2.1_30s_bio_19.tif")
+predictors <- stack(bio1, bio2, bio3, bio4, bio5, bio6, bio7, bio8, bio9, bio10, bio11, bio12, bio13, bio14, bio15, bio16, bio17, bio18, bio19)
+names (predictors)
+plot(CzechBorders)
+
+CzechBorders <- shapefile("CZE_adm0.shp") #loading the territoty 
+
+#crop predictord by our territory
+predictors <- crop(predictors, CzechBorders)
+plot(predictors)
+plot(predictors, 1) #show plot of the first predictor
+
+#our data (created shp file in QGIS from csv)
+points<- shapefile("czhlenky.shp")
+points (points, col="blue")
+#this is all evidence of the presence of occurrence
+Lamproderma <- data.frame(points@coords)
+occ <- rep(1,nrow(Lamproderma))
+occurrence <-cbind(Lamproderma, occ)
+occurrence
+names(occurrence)[1] <- "lon"
+names(occurrence)[2] <- "lat"
+#transform df into Spatial Points df
+coordinates(occurrence) <- c("lon", "lat")
+#VIF calculation
+library(usdm)
+v <- vifstep (predictors)
+v
+pred <- exclude(predictors, v)
+pred
+#model
+premodel <- sdmData(fromula=occ~.,train = occurrence, predictors = pred, bg =
+               list(method="gRandom", n=100))
+model <- sdm(occ~.,premodel, methods=c("Bioclim","Maxent","RF"), replication = "boot",
+         test.percent=20, n=5)
+model
+p1 <- predict(model,pred,filename="pr1.img", overwrite=TRUE)
+plot(p1)
+#view by index
+plot(p1[[13]])
+# Visualisation
+RF <- ensemble(model, pred, filename = "RF.img",
+               setting=list(id=c(11,12,13,14,15),method="weighted", stat="tss", opt=2))
+plot(RF)
+points(occurrence,col="blue")
+enseb1.df = as.data.frame(RF, xy=TRUE)
+head(enseb1.df)
+CZ = st_as_sf(CzechBorders) # convert to simple features object
+ggplot()+
+  geom_raster(aes(x=x,y=y, fill=RF),data=enseb1.df)+
+  geom_sf(fill='transparent', colour = "white", data=CZ)+
+  scale_fill_viridis_c('')+
+  labs(x="", y = "",
+       title = "RF")+
+  theme_minimal()
+ggsave("RF_30_sec_7_preds.png", width=14, height=10, unit="cm", dpi=300)
+#BIOCLIM visualization
+Bioclim <- ensemble(model, pred, filename = "bioclim.img",
+               setting=list(id=c(1,2,3,4,5),method="weighted", stat="tss", opt=2))
+plot(Bioclim)
+points(occurrence,col="blue")
+# Visualisation
+maxentpred.df =  as.data.frame(Bioclim, xy=TRUE)
+head(maxentpred.df)
+ggplot()+
+  geom_raster(aes(x=x,y=y,fill=bioclim),data=maxentpred.df)+
+  geom_sf(fill='transparent', colour = "white", data=CZ)+
+  scale_fill_viridis_c('')+
+  labs(x="", y = "",
+       title = "BIOCLIM",
+       caption = "Data: Bioclim 30 sec")+
+  theme_minimal()
