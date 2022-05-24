1f0330b82ac05aaa45b8c2bca27032168c13a8b6
Juliana Leshchenko <68852795+JuliianaLeshchenko@users.noreply.github.com>
2022-05-24 11:17:00 +0200

    Update How high are nivicolous myxomycetes in CZ.R

--- How high are nivicolous myxomycetes in CZ.R
+++ How high are nivicolous myxomycetes in CZ.R
@@ -163,3 +163,7 @@ ggplot()+
        title = "BART")+
   theme_minimal()
 ggsave("BART_30_sec_3_preds.jpg", width=14, height=10, unit="cm", dpi=300)
+plot.new()
+polygon(x=c(0,1,1,0),y=c(0,0,0.5,0.5), col="#FFDD00")
+polygon(x=c(0,1,1,0),y=c(0.5,0.5,1,1), col="#0057B7")
+text(0.5, 0.25, labels="Слава Україні", cex=2)
