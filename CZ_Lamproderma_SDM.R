b3a399c8708567613de2de39763470fe4140a86c
Juliana Leshchenko <68852795+JuliianaLeshchenko@users.noreply.github.com>
2022-11-16 13:29:52 +0100

    Update CZ_Lamproderma_SDM.R

--- CZ_Lamproderma_SDM.R
+++ CZ_Lamproderma_SDM.R
@@ -80,3 +80,106 @@ m <- sdm(occ~.,d, methods="Bioclim", replication = "boot",
          test.percent=20, n=5)
 m
 p1 <- predict(m,significantPred,filename="pr1.img", overwrite=TRUE)
+
+###MAXENT
+rm(list = ls())
+# Loadind tools and data
+library(dismo)
+library(sp)
+library(rgeos)
+library(rgdal)
+library(maptools)
+library(rJava)
+library(gbm)
+library(raster)
+library(sf)
+library(ggplot2)
+library(tidyr)
+library(usdm)
+#import data from worldclim (previsly croped in QGIS):
+bio1 <- raster("wc2.1_10m_bio_1.tif")
+bio2 <- raster("wc2.1_10m_bio_2.tif")
+bio3 <- raster("wc2.1_10m_bio_3.tif")
+bio4 <- raster("wc2.1_10m_bio_4.tif")
+bio5 <- raster("wc2.1_10m_bio_5.tif")
+bio6 <- raster("wc2.1_10m_bio_6.tif")
+bio7 <- raster("wc2.1_10m_bio_7.tif")
+bio8 <- raster("wc2.1_10m_bio_8.tif")
+bio9 <- raster("wc2.1_10m_bio_9.tif")
+bio10 <- raster("wc2.1_10m_bio_10.tif")
+bio11 <- raster("wc2.1_10m_bio_11.tif")
+bio12 <- raster("wc2.1_10m_bio_12.tif")
+bio13 <- raster("wc2.1_10m_bio_13.tif")
+bio14 <- raster("wc2.1_10m_bio_14.tif")
+bio15 <- raster("wc2.1_10m_bio_15.tif")
+bio16 <- raster("wc2.1_10m_bio_16.tif")
+bio17 <- raster("wc2.1_10m_bio_17.tif")
+bio18 <- raster("wc2.1_10m_bio_18.tif")
+bio19 <- raster("wc2.1_10m_bio_19.tif")
+elev <-raster('wc2.1_10m_elev.tif')
+predictors <- stack(bio1,bio2,bio3,bio4,bio5,bio6,bio7,bio8,bio9,bio10,bio11,bio12,bio12,bio13,bio14,bio15,bio16,bio17,bio18,bio19,elev)
+names (predictors)
+oc <- readOGR(dsn = ".", layer = "greatdataset")
+EU <- readOGR(dsn = ".", layer = "BiogeoRegions2016")
+summary(EU)
+EUt <- spTransform(EU, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+# Cleaning data
+str(oc)
+#View(oc@data)
+oc <- remove.duplicates(oc)
+plot(predictors)
+plot(predictors, 1)
+points (oc, col="blue")
+#dev.off()
+m2_maxent <- maxent(predictors, oc@coords)
+maxentpred <- predict(predictors, m2_maxent)
+plot(maxentpred)
+points(oc, col="blue")
+# Simulating background data
+presvals <- raster::extract(predictors, oc)
+backgr <- spsample (oc, 1000, type = "random")
+plot(predictors, 1)
+points (oc, col="blue")
+points (backgr, col="red")
+backgvals <- raster::extract(predictors, backgr)
+pr <- c(rep(1, nrow(presvals)), rep(0, nrow(backgvals)))
+sdmdata <- data.frame(cbind(pr, rbind(presvals, backgvals)))
+View(sdmdata)
+sdmdata = na.omit(sdmdata)
+rm(pr, files)
+group <- kfold(oc, 5) # randomly divide the data into 5 equal parts
+pres_train <- oc[group != 1, ] # 80% (parts 1-5) as training data
+pres_test <- oc[group == 1, ] # 20% (part 1) as testing data
+group1 <- kfold(backgr, 5)
+backg_train <- backgr[group1 != 1, ]
+backg_test <- backgr[group1 == 1, ]
+plot(predictors, 1)
+points(pres_train, pch= "+", cex=1.5, col="red")
+points(backg_train, pch="-", cex=1.5, col="blue")
+points(pres_test, pch="+", cex=1.5, col="maroon")
+points(backg_test, pch="-", cex=1.5, col="black")
+m2_maxent <- maxent(predictors, pres_train)
+#visualy evaluate the significance of the predictors
+plot(m2_maxent)
+e_maxent <- evaluate(pres_test, backg_test, m2_maxent, predictors)
+e_maxent
+plot(e_maxent, "ROC")
+maxentpred <- predict(predictors, m2_maxent)
+plot (maxentpred)
+tr <- threshold(e_maxent, "spec_sens")
+plot(maxentpred > tr, main="presence/absence")
+# Visualisation
+maxentpred.df =  as.data.frame(maxentpred, xy=TRUE) %>% drop_na()
+head
+
+# Plotting
+EU_plot = st_as_sf(EUt) # convert to simple features object
+ggplot()+
+  geom_raster(aes(x=x,y=y,fill=layer),data=maxentpred.df)+
+  geom_sf(fill='transparent', colour = "white", data=EU_plot)+
+  scale_fill_viridis_c('')+
+  labs(x="", y = "",
+       title = "Maxent",
+       caption = "Data: Bioclim and elevation 10 min, GBIF, literature extracted points, L. ovoideum, L. aeneum, L. preudomaculatum, L. splendens, L. arcyrioides, L. sauteri")+
+  theme_minimal()
+
